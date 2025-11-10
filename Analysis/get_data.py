f2ffa2edaf1b72d3dbc5f2a806b1f2fbeb3d7d97
Jonah Fein <jonahafein@Jonahs-MacBook-Air-4.local>
2025-11-09 23:06:34 -0500

    f

--- Analysis/get_data.py
+++
@@ -1,5 +0,0 @@
-from scripts.Azure_Database import database
-
-sleep_raw = database()
-sleep_df_raw = sleep_raw.query("SELECT * FROM sleep_raw order by dateOfSleep desc")
-print(sleep_df_raw.head())
